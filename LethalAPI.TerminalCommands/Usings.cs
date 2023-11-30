52ea37fb570d38cc94921ebcd2fc92fc40887bee
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-11-30 12:15:21 -0600

    Add Log usings

---
+++ LethalAPI.TerminalCommands/Usings.cs
@@ -0,0 +1,11 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Usings.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#pragma warning disable SA1200 // Using statements should appear in a namespace declaration.
+
+global using Log = LethalAPI.Core.Log;
+
